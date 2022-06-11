68bf9c0acf2bea03ed71ccbc44091b351b3bbb77
Ahmad Haydar Alkaf <alkaf.haydar@gmail.com>
2022-06-12 02:23:20 +0700

    separated read from update

--- app.js
+++ app.js
@@ -2,7 +2,6 @@
 const express = require('express');
 const app = express();
 const studentUpdate = require('./src/controller/update');
-const studentRead = require('./src/controller/read');
 const bodyParser = require('body-parser');
 
 // use body parser
@@ -10,7 +9,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post('/', studentUpdate.postStudent);
-app.get('/:npm', studentRead.getStudent);
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000')
