cb5410bb259fca92d61e2919652468c6b845c6d1
Ahmad Haydar Alkaf <alkaf.haydar@gmail.com>
2022-06-12 01:28:17 +0700

    delete 1 useless comment

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-//app get hello world
 app.post('/', studentUpdate.postStudent);
 app.get('/:npm', studentRead.getStudent);
 
